5a5bf4324a2c083485d17c919fab451d8a56ef50
Fernada_Kelly <fernanda.kelly@intelitrader.com.br>
2023-03-07 17:57:05 -0300

    🐛: Ajusta responsividade.

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -7,15 +7,31 @@ import {
     DiCss3,
     DiGithub,
     DiJavascript,
-    DiGit
+    DiGit,
+    DiAngularSimple,
+    DiDatabase,
+    DiDotnet,
+    DiJava,
+    DiJira,
+    DiPython,
+    DiReact
 } from 'react-icons/di'
 
 const technologies = [
     { id: "html", name: "HTML5", icon: <DiHtml5 /> },
     { id: "css", name: "CSS3", icon: <DiCss3 /> },
+    { id: "javascript", name: "JavaScript", icon: <DiJavascript /> },
+    { id: "react", name: "React JS", icon: <DiReact /> },
+    { id: "angular", name: "Angular", icon: <DiAngularSimple /> },
+
+    { id: "dotnet", name: ".NET", icon: <DiDotnet /> },
+    { id: "java", name: "Java", icon: <DiJava /> },
+    { id: "python", name: "Python", icon: <DiPython /> },
+    { id: "sql", name: "SQL", icon: <DiDatabase /> },
+
     { id: "git", name: "Git", icon: <DiGit /> },
     { id: "github", name: "GitHub", icon: <DiGithub /> },
-    { id: "javascript", name: "JavaScript", icon: <DiJavascript /> }
+    { id: "jira", name: "Jira", icon: <DiJira /> }
 ];
 
 const TechnologiesContainer = () => {
